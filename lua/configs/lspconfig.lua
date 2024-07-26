e355eaf135fc5348befc42cab1340cf615f8eecc
T0nd0Tara <5632815@gmail.com>
2024-07-26 20:50:51 +0300

    my config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,23 +1,22 @@
--- EXAMPLE 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
 
--- lsps with default config
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "clangd", "pyright" }
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
-    on_init = on_init,
     capabilities = capabilities,
   }
 end
 
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+-- lspconfig.clangd.setup {
+--   init_options = {
+--     fallbackFlags = {'--std=c++20'}
+--   },
+-- }
+-- 
+-- lspconfig.pyright.setup { blabla}
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,21 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("t", "jk", "<C-\\><C-n>", { desc = "exit terminal" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--  format with conform
+map("n", "<leader>fm", function() require("conform").format() end, { desc = "formatting" })
+
+map("n", "<leader>h", "<cmd> NvCheatsheet <CR>", { desc = "Open NvCheatsheet"})
+map("n", "<leader>gr", "<cmd> Telescope live_grep <CR>", { desc = "Live grep" })
+-- Debug
+map("n", "<leader>b", "<cmd>a lua require'dap'.toggle_breakpoint() <CR>", { desc = "Toggle Breakpint"})
+map("n", "<leader>d", "<cmd> lua require'dap'.continue() <CR>", { desc = "Start Debugger"})
+map("n", "<C-d-n>", "<cmd> lua require'dap'.step_over() <CR>", { desc = "Step Over"})
+map("n", "<C-d-i>", "<cmd> lua require'dap'.step_into() <CR>", { desc = "Step Over"})
+map("n", "<C-d>r", "<cmd> lua require'dap'.repl.open() <CR>", { desc = "Step Over"})
+
+map("v", ">", ">gv", { desc = "indent"})
+
+return M
