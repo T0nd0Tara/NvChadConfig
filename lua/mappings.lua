8a7c7279aa98f2a56f26c131551b40ce7b55bf88
T0nd0Tara <5632815@gmail.com>
2024-10-15 21:55:55 +0300

    add data view shortcut and find files shortcut

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,8 +7,8 @@ map("n", "<leader>md", "<cmd> MarkdownPreviewToggle <CR>", { desc = "live previe
 --  format with conform
 map("n", "<leader>fm", function() require("conform").format() end, { desc = "formatting" })
 
-map("n", "<leader>h", "<cmd> NvCheatsheet <CR>", { desc = "Open NvCheatsheet"})
 map("n", "<leader>gr", "<cmd> Telescope live_grep <CR>", { desc = "Live grep" })
+map("n", "<leader>ff", "<cmd> Telescope find_files <CR>", { desc = "Find Files" })
 -- Debug
 map("n", "<leader>db", "<cmd> lua require'dap'.toggle_breakpoint() <CR>", { desc = "Toggle Breakpint"})
 map("n", "<leader>dd", "<cmd> lua require'dap'.continue() <CR>", { desc = "Start Debugger"})
@@ -16,6 +16,9 @@ map("n", "<leader>dn", "<cmd> lua require'dap'.step_over() <CR>", { desc = "Step
 map("n", "<leader>di", "<cmd> lua require'dap'.step_into() <CR>", { desc = "Step Into"})
 map("n", "<leader>dr", "<cmd> lua require'dap'.repl.open() <CR>", { desc = "Open REPL"})
 
+map("n", "<leader>dv", "<cmd> DataViewer <CR>", { desc = "Open Data Viewer"})
+
 map("v", ">", ">gv", { desc = "indent"})
+map("v", "<", "<gv", { desc = "indent"})
 
 return M
