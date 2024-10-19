a980fc2c39b86123ea80ea77410d778ab6f81470
T0nd0Tara <5632815@gmail.com>
2024-10-19 20:05:16 +0300

    add lsp-lens

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -123,6 +123,30 @@ local plugins = {
       "kkharji/sqlite.lua", -- Optional, sqlite support
     }
   },
+  {
+    'VidocqH/lsp-lens.nvim',
+    lazy = false,
+    config = function()
+      local SymbolKind = vim.lsp.protocol.SymbolKind
+      require("lsp-lens").setup({
+        enable = true,
+        include_declaration = false,      -- Reference include declaration
+        sections = {                      -- Enable / Disable specific request, formatter example looks 'Format Requests'
+          definition = false,
+          references = true,
+          implements = true,
+          git_authors = true,
+        },
+        ignore_filetype = {
+          "prisma",
+        },
+        -- Target Symbol Kinds to show lens information
+        target_symbol_kinds = { SymbolKind.Function, SymbolKind.Method, SymbolKind.Interface },
+        -- Symbol Kinds that may have target symbol kinds as children
+        wrapper_symbol_kinds = { SymbolKind.Class, SymbolKind.Struct },
+      })
+    end,
+  }
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
