a0e8f3f2f7d7adbdb7f3182b0d5226a4dcaf4a52
T0nd0Tara <5632815@gmail.com>
2024-10-20 09:20:42 +0300

    include everything in plugin-config

--- lua/myinit.lua
+++ lua/myinit.lua
@@ -8,3 +8,15 @@
 
 
 vim.cmd [[ source ~/.vimrc ]]
+
+-- loading all of the plugin configs
+local configs = vim.api.nvim_get_runtime_file('lua/plugins/plugin-configs/*.lua', true)
+if configs ~= nil then
+  for _, filename in ipairs(configs) do
+    dofile(filename)
+  end
+else
+  vim.print("Error in loading plugin configs")
+end
+
+-- dofile('/home/amirs/.config/nvim/lua/plugins/plugin-configs/lsp-lens.lua')
