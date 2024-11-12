c3c599e3a3f6d1ca5f31ca6c1defbad8f091367a
T0nd0Tara <5632815@gmail.com>
2024-11-12 09:12:08 +0200

    finish playing with the status line (i think)

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -15,32 +15,30 @@ M.base46 = {
   hl_add = highlights.add,
 }
 
+local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+
 -- stole it from the ui module of NvChad. as I was unable to use it otherwise for some reason
-local file_info = function()
+local file_icon = function()
   local icon = "󰈚"
   local path = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0))
   local name = (path == "" and "Empty") or path:match "([^/\\]+)[/\\]*$"
 
-  if name ~= "Empty" then
-    local devicons_present, devicons = pcall(require, "nvim-web-devicons")
-
-    if devicons_present then
-      local ft_icon = devicons.get_icon(name)
-      icon = (ft_icon ~= nil and ft_icon) or icon
-    end
+  if name ~= "Empty" and devicons_present then
+    local ft_icon = devicons.get_icon(name)
+    icon = (ft_icon ~= nil and ft_icon) or icon
   end
 
-  return { icon, path }
+  return icon
 end
 
 M.ui = {
   statusline = {
-   order = { "mode", "file","git", "%=", "lsp_msg", "clock", "%=", "lsp", "cwd" },
+   order = { "mode", "file_path","git", "%=", "clock", "%=", "lsp_msg", "lsp", "cwd"},
     modules = {
       clock = "%{strftime('%c')}",
       file_path = function()
-        local file_logo, path = file_info()
-        return "%#StText#" .. file_logo .. "%F"
+        local icon = file_icon()
+        return "%#St_file#" .. "%F " .. icon .. " %#St_file_sep#"
       end,
     }
   }
