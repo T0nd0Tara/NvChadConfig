3f8bd4417a7753a38538f4782ab31aae791e0828
T0nd0Tara <5632815@gmail.com>
2024-11-21 09:30:07 +0200

    get git contributions faster and more properly show them

--- scripts/get_github_contributions.sh
+++ scripts/get_github_contributions.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-USER=$1
+GITHUB_USER=$1
 
-if [ -z $USER ]; then 
+if [ -z $GITHUB_USER ]; then 
+  echo "Error: a github user is required"
   exit 1
 fi
 
-CACHE_FILE=~/.config/nvim/cache/github-contributions-$USER.txt
+CACHE_FILE=~/.config/nvim/cache/github-contributions-$GITHUB_USER.json
 FIRST_CACHE="[ ! -f $CACHE_FILE ]"
 
 fetch_data()
@@ -18,11 +19,10 @@ fetch_data()
   TIME_DIFF=$(expr $CURTIME - $LAST_CHANGE)
 
   if [ $TIME_DIFF -gt $STALE_TIME ] || $FIRST_CACHE; then
-    curl -s "https://github-contributions-api.jogruber.de/v4/$USER?y=$(date -d "1 year ago" +%Y)&y=$(date +%Y)" \
-      | jq '.contributions' \
-      | jq --arg start $(date -d "1 year ago" +%Y-%m-%d) --arg end $(date +%Y-%m-%d) '.[] | select((.date >= $start) and (.date <= $end))' \
-      | jq -s 'sort_by(.date) | map(.level) | . as $array | reduce range(0; length; 7) as $i ({}; . + {($i/7+1 | tostring): $array[$i:$i+7] })' \
-      > $CACHE_FILE 
+    curl -s "https://github-contributions-api.jogruber.de/v4/$GITHUB_USER?y=$(date -d "1 year ago" +%Y)&y=$(date +%Y)" \
+      | jq --arg start $(date -d "1 year ago" +%Y-%m-%d) --arg end $(date +%Y-%m-%d) \
+        '.contributions | [ .[] | select((.date >= $start) and (.date <= $end)) ] | sort_by(.date) | (.[0].date | strptime("%Y-%m-%d") | strftime("%w") | tonumber) as $wd | map(.level) | ([range(0, $wd) ] | map(0)) + . | . as $array | reduce range(0; length; 7) as $i ({}; . + {($i/7+1 | tostring): $array[$i:$i+7] })' \
+      >| $CACHE_FILE 
   fi
 }
 
